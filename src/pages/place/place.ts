b1c5fb3d496f3066e1ca706be438a0b4ca25573b
zaqwsx55 <zaqwsx@poczta.fm>
2017-12-15 14:23:57 +0100

    Nowa strona: Place

---
+++ src/pages/place/place.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { IonicPage, ViewController } from 'ionic-angular';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, 
+  CameraPosition, MarkerOptions, Marker } from '@ionic-native/google-maps';
+
+@IonicPage()
+@Component({
+  selector: 'page-place',
+  templateUrl: 'place.html',
+})
+export class PlacePage {
+
+  map: GoogleMap;
+
+  constructor(private viewCtrl: ViewController) {}
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    // Wait the MAP_READY before using any methods
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      console.log('Map is ready!');
+      // Now you can use all mathods safely
+      this.map.addMarker({
+        title: 'Ionic',
+        icon: 'blue',
+        animation: 'DROP',
+        position: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        }
+      }).then(marker => {
+        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+          alert('clicked');
+        });
+      });
+    });
+
+  }
+
+  onDismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+}
