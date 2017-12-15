a27bfd6ccffb58a6b42b2c53ecc1bf648af43b70
zaqwsx55 <zaqwsx@poczta.fm>
2017-12-15 14:22:23 +0100

    Dodanie nowych modułów: IonicStrageModule, Geolocation i GoogleMaps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
+import { GoogleMaps } from '@ionic-native/google-maps';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { PlacesPage } from '../pages/places/places';
 import { NewPlacePage } from '../pages/new-place/new-place';
+import { PlacePage } from '../pages/place/place';
 import { PlacesService } from '../services/places.service';
 
 @NgModule({
@@ -15,22 +19,27 @@ import { PlacesService } from '../services/places.service';
     MyApp,
     HomePage,
     PlacesPage,
-    NewPlacePage
+    NewPlacePage,
+    PlacePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
     PlacesPage,
-    NewPlacePage
+    NewPlacePage,
+    PlacePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    GoogleMaps,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     PlacesService
   ]
