05b0afa86f0c127a29c47437fb625d013dbdc66b
zaqwsx55 <zaqwsx@poczta.fm>
2017-12-15 14:23:10 +0100

    Dodanie IonicStorage

--- src/services/places.service.ts
+++ src/services/places.service.ts
@@ -1,17 +1,29 @@
+import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
 import { Place } from '../models/place';
 
+@Injectable()
 export class PlacesService {
 
     private places: Place[] = [];
 
+    constructor(private storage: Storage) {}
+
     addPlace(place: Place) {
         this.places.push(place);
+        this.storage.set('places', this.places);
     }
 
     getPlaces() {
-        return this.places;
+        return this.storage.get('places').then((places) => {
+            if (places) {
+                this.places = places
+            } else {
+                this.places = [];
+            }
+            return this.places;
+        })
     }
 
 }
