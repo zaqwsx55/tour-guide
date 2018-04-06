d289c0ce47807209c3977da186b21761eacd9126
zaqwsx55 <zaqwsx@poczta.fm>
2018-04-06 14:36:17 +0200

    Sprawdzanie bieżącej pozycji

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 
 declare var google;
 
@@ -12,27 +13,203 @@ export class MapPage {
 
   @ViewChild('map') mapElement;
   map: any;
+  position: Geoposition;
 
   //public map: google.maps.Map = null;
 
-  constructor(private navCtrl: NavController, private navParams: NavParams) {
-  }
+  constructor(private navCtrl: NavController,
+              private navParams: NavParams,
+              private geolocation: Geolocation) {}
 
   ionViewDidLoad() {
-    this.initMap();
+
+    console.log('ion view did load');
+
+    this.geolocation.watchPosition().subscribe((position) => {
+      this.position = position;
+      this.initMap();
+    });
+
+    
   }
 
   initMap() {
 
-    let latLng = new google.maps.LatLng(50.0616124, 19.9391191);
+    // let latLng = new google.maps.LatLng(50.0616124, 19.9391191);
+    let latLng = new google.maps.LatLng(this.position.coords.latitude, this.position.coords.longitude);
 
     let mapOptions = {
       center: latLng,
-      zoom: 19,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      zoom: 20,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f5f5f5"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#616161"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#f5f5f5"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#bdbdbd"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#eeeeee"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e5e5e5"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#ffffff"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dadada"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#616161"
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e5e5e5"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#eeeeee"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#c9c9c9"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        }
+      ]
     };
 
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map
+    });
 
   }
 
